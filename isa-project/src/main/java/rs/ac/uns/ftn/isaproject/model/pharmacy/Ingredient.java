50741c45d853285668743084a41baa33cb0ebb3e
draganacarapic <dragana.carapic98@uns.ac.rs>
2021-01-27 22:09:12 +0100

    feat: add pharmacy repositories

--- isa-project/src/main/java/rs/ac/uns/ftn/isaproject/model/pharmacy/Ingredient.java
+++ isa-project/src/main/java/rs/ac/uns/ftn/isaproject/model/pharmacy/Ingredient.java
@@ -7,7 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity 
-class Ingredient {
+public class Ingredient {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
