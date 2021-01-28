83f16a5d5d3657ae88c6509faf52dc37e76bb0f7
Sladjana Savkovic <sladjana.savkovic97@uns.ac.rs>
2021-01-28 20:37:13 +0100

    refactor:delete comments

--- isa-project/src/main/java/rs/ac/uns/ftn/isaproject/controller/users/PatientController.java
+++ isa-project/src/main/java/rs/ac/uns/ftn/isaproject/controller/users/PatientController.java
@@ -16,10 +16,4 @@ public class PatientController {
 		this.patientService = patientService;
 	}
 	
-	/*@GetMapping("/examined/{id}")
-	public ResponseEntity<Collection<ExaminedPatientDTO>> findAllExaminedByDoctor(@PathVariable int id){
-		Collection<ExaminedPatientDTO> examinedPatientDTOs = 
-				ExaminedPatientMapper.toExaminedPatientDTOs(patientService.findAllExaminedByDoctor(id));
-		return new ResponseEntity<Collection<ExaminedPatientDTO>>(examinedPatientDTOs,HttpStatus.OK);
-	}*/
 }
