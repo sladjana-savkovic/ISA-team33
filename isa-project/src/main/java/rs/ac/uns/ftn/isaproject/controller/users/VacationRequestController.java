dbf5284667e67a42434da9c4e4645edc779fa6ed
Sladjana Savkovic <sladjana.savkovic97@uns.ac.rs>
2021-01-28 21:23:49 +0100

    feat:add vacation request by doctor

--- isa-project/src/main/java/rs/ac/uns/ftn/isaproject/controller/users/VacationRequestController.java
+++ isa-project/src/main/java/rs/ac/uns/ftn/isaproject/controller/users/VacationRequestController.java
@@ -1,8 +1,14 @@
 package rs.ac.uns.ftn.isaproject.controller.users;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import rs.ac.uns.ftn.isaproject.dto.AddVacationRequestDTO;
 import rs.ac.uns.ftn.isaproject.service.users.VacationRequestService;
 
 @RestController
@@ -14,5 +20,11 @@ public class VacationRequestController {
 	@Autowired
 	public VacationRequestController(VacationRequestService vacationService) {
 		this.vacationService = vacationService;
+	}
+	
+	@PostMapping(consumes = "application/json")
+	public ResponseEntity<Void> Add(@RequestBody AddVacationRequestDTO vacationRequestDTO){
+		vacationService.Add(vacationRequestDTO);
+		return new ResponseEntity<Void>(HttpStatus.CREATED);
 	}
 }
--- isa-project/src/main/java/rs/ac/uns/ftn/isaproject/service/users/VacationRequestService.java
+++ isa-project/src/main/java/rs/ac/uns/ftn/isaproject/service/users/VacationRequestService.java
@@ -1,5 +1,8 @@
 package rs.ac.uns.ftn.isaproject.service.users;
 
+import rs.ac.uns.ftn.isaproject.dto.AddVacationRequestDTO;
+
 public interface VacationRequestService {
 
+	void Add(AddVacationRequestDTO vacationRequestDTO);
 }
