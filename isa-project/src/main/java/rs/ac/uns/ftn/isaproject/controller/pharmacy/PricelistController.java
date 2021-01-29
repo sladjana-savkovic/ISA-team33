1742053a0390911f36a1f3d2c036f3f85e7d9e5b
draganacarapic <dragana.carapic98@uns.ac.rs>
2021-01-29 12:14:22 +0100

    feat: create new pricelist item

--- isa-project/src/main/java/rs/ac/uns/ftn/isaproject/controller/pharmacy/PricelistController.java
+++ isa-project/src/main/java/rs/ac/uns/ftn/isaproject/controller/pharmacy/PricelistController.java
@@ -1,9 +1,14 @@
 package rs.ac.uns.ftn.isaproject.controller.pharmacy;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import rs.ac.uns.ftn.isaproject.dto.PricelistDTO;
 import rs.ac.uns.ftn.isaproject.service.pharmacy.PricelistService;
 
 @RestController
@@ -15,5 +20,15 @@ public class PricelistController {
 	@Autowired
 	public PricelistController(PricelistService pricelistService) {
 		this.pricelistService = pricelistService;
+	}
+	
+	@PostMapping(consumes = "application/json")
+	public ResponseEntity<Void> add(@RequestBody PricelistDTO pricelistDTO) {
+		try {
+			pricelistService.save(pricelistDTO);
+			return new ResponseEntity<Void>(HttpStatus.CREATED);
+		}catch (Exception e) {
+			return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
+		}
 	}
 }
--- isa-project/src/main/java/rs/ac/uns/ftn/isaproject/service/pharmacy/PricelistService.java
+++ isa-project/src/main/java/rs/ac/uns/ftn/isaproject/service/pharmacy/PricelistService.java
@@ -1,5 +1,8 @@
 package rs.ac.uns.ftn.isaproject.service.pharmacy;
 
+import rs.ac.uns.ftn.isaproject.dto.PricelistDTO;
+
 public interface PricelistService {
 
+	void save(PricelistDTO pricelistDTO);
 }
