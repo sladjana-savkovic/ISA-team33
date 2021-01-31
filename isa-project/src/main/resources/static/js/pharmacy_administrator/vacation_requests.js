bce6c97927954a0896df8b04d25aa462e3a6127d
draganacarapic <dragana.carapic98@uns.ac.rs>
2021-02-01 00:52:22 +0100

    gui: accept vacation request

---
+++ isa-project/src/main/resources/static/js/pharmacy_administrator/vacation_requests.js
@@ -0,0 +1,65 @@
+var pharmacyAdminId = 6;
+var pharmacyId;
+$(document).ready(function () {
+	
+	$.ajax({
+		type:"GET", 
+		url: "/api/pharmacy-admin/" + pharmacyAdminId,
+		contentType: "application/json",
+		success:function(admin){
+			pharmacyId = admin.pharmacyId;
+			
+			$.ajax({
+				type:"GET", 
+				url: "/api/vacation/pharmacy/" + pharmacyId,
+				contentType: "application/json",
+				success:function(vacations){
+					
+					for(i = 0; i < vacations.length; i++){
+						addVacationRequest(vacations[i]);
+					}
+					
+				},
+				error:function(){
+					console.log('error getting vacation requests');
+				}
+				});
+			
+		},
+		error:function(){
+			console.log('error getting pharmacy administrator');
+		}
+	});
+	
+});
+
+function addVacationRequest(vacation){
+	 let row = $('<tr><td>'+ vacation.doctorSurname +'</td><td>' + vacation.typeOfDoctor + '</td><td>' + vacation.startDate + '</td><td>' + vacation.endDate + '</td><td>' + '<button name="rejectButton" type = "button" class="btn btn-success float-right" id="' + vacation.id + '" onclick="acceptRequest(this.id)">Accept</button>' + '</td><td>' + '<button name="rejectButton" type = "button" class="btn btn-danger float-right" id="' + vacation.id + '" onclick="rejectRequest(this.id)">Reject</button>' + '</td></tr>');	
+	$('#vacations').append(row);
+};
+
+function rejectRequest(id){
+			
+};
+
+function acceptRequest(id){
+	
+			$.ajax({
+				type:"PUT", 
+				url: "/api/vacation/" + id,
+				contentType: "application/json",
+				success:function(){
+					location.reload();
+					let alert = $('<div class="alert alert-success alert-dismissible fade show m-1" role="alert">Successfully accept vacation request.'
+						+'<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>' + '</div >')
+					$('#div_alert').append(alert);
+					return;
+				},
+				error:function(){
+					let alert = $('<div class="alert alert-danger alert-dismissible fade show m-1" role="alert">Error accept vacation request.'
+						+'<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>' + '</div >')
+					$('#div_alert').append(alert);
+					return;
+				}
+			});
+};
