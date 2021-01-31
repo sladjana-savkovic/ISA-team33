d82206d1fa9320c2409bee81d118d7d6c62c501e
Sladjana Savkovic <sladjana.savkovic97@uns.ac.rs>
2021-01-31 18:49:48 +0100

    feat:add methods for check availability and reduce quantity

--- isa-project/src/main/java/rs/ac/uns/ftn/isaproject/service/pharmacy/DrugQuantityPharmacyService.java
+++ isa-project/src/main/java/rs/ac/uns/ftn/isaproject/service/pharmacy/DrugQuantityPharmacyService.java
@@ -2,4 +2,6 @@ package rs.ac.uns.ftn.isaproject.service.pharmacy;
 
 public interface DrugQuantityPharmacyService {
 
+	boolean checkDrugAvailability(int drugId, int pharmacyId);
+	boolean reduceDrugQuantity(int drugId, int pharmacyId, int quantity);
 }
--- isa-project/src/main/java/rs/ac/uns/ftn/isaproject/service/pharmacy/DrugQuantityPharmacyServiceImpl.java
+++ isa-project/src/main/java/rs/ac/uns/ftn/isaproject/service/pharmacy/DrugQuantityPharmacyServiceImpl.java
@@ -1,9 +1,13 @@
 package rs.ac.uns.ftn.isaproject.service.pharmacy;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Collection;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import rs.ac.uns.ftn.isaproject.model.pharmacy.DrugQuantityPharmacy;
 import rs.ac.uns.ftn.isaproject.repository.pharmacy.DrugQuantityPharmacyRepository;
 
+@Service
 public class DrugQuantityPharmacyServiceImpl implements DrugQuantityPharmacyService{
 
 	private DrugQuantityPharmacyRepository quantityPharmacyRepository;
@@ -11,5 +15,32 @@ public class DrugQuantityPharmacyServiceImpl implements DrugQuantityPharmacyServ
 	@Autowired
 	public DrugQuantityPharmacyServiceImpl(DrugQuantityPharmacyRepository quantityPharmacyRepository) {
 		this.quantityPharmacyRepository = quantityPharmacyRepository;
+	}
+
+	@Override
+	public boolean checkDrugAvailability(int drugId, int pharmacyId) {
+		Collection<DrugQuantityPharmacy> quantityPharmacies = quantityPharmacyRepository.findAll();
+		
+		for(DrugQuantityPharmacy quantity:quantityPharmacies) {
+			if(quantity.getDrug().getId() == drugId && quantity.getPharmacy().getId() == pharmacyId && quantity.getQuantity() > 0)
+				return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean reduceDrugQuantity(int drugId, int pharmacyId, int quantity) {
+		Collection<DrugQuantityPharmacy> quantityPharmacies = quantityPharmacyRepository.findAll();
+		
+		for(DrugQuantityPharmacy drugQuantity:quantityPharmacies) {
+			if(drugQuantity.getDrug().getId() == drugId && drugQuantity.getPharmacy().getId() == pharmacyId) {
+				if(drugQuantity.getQuantity() - quantity > 0) {
+					drugQuantity.setQuantity(drugQuantity.getQuantity() - quantity);
+					quantityPharmacyRepository.save(drugQuantity);
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 }
