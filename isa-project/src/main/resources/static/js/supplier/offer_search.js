6f2da8cb10dba1925618040373e5f1f12bdcf272
Jelena Zeljko <jelena_zeljko@uns.ac.rs>
2021-02-08 00:19:36 +0100

    gui: the supplier can see his offers (js file)

--- isa-project/src/main/resources/static/js/supplier/offer_search.js
+++ isa-project/src/main/resources/static/js/supplier/offer_search.js
@@ -48,7 +48,7 @@ function searchOffer() {
 
 function getAllOffersBySupplier() {		
     $.ajax({
-        //url: '/api/drug',
+        url: '/api/drug-offer/all/' + supplierID + '/supplier',
         type: 'GET',
         dataType: 'json',
         processData: false,
@@ -62,7 +62,7 @@ function getAllOffersBySupplier() {
             }
             else {
                 for (let i = 0; i < offers.length; i++) {
-                    addOfferRow(offers[i]);
+                    addOfferRow(offers[i], i);
                 }
                 $("#loading").hide();
             }
@@ -76,95 +76,31 @@ function getAllOffersBySupplier() {
 }
 
 
-function addOfferRow(offer) {
+function addOfferRow(offer, i) {
 		
 	offerMap[offer.id] = offer;
-	
-	button = '<div class="card-footer">'
-		+ '<button type = "button" class="btn btn-outline-primary float-left"'
-		+ 'id="' + offer.id + '" onclick="showSpecification(this.id)"'
-		+ '>Specification</button >'
-		+ '<button type = "button" class="btn btn-outline-primary float-left"'
-		+ 'id="' + offer.id + '" onclick="showSubstituteDrugs(this.id)"'
-		+ '>Substitute</button >'		
-		+ '</div >'; 
-			
+				
     let divElement = $(
         '<div class="row">'
         + '<div class="col mb-4">'
         + '<div class="card">'
         + '<div class="card-header bg-info text-white">'
-        + '<h5 class="card-title mb-0">' + offer.name + '</h5></div>'
+        + '<h5 class="card-title mb-0"> Offer (ID = ' + offer.drugOfferDTO.id + ')</h5></div>'
         + '<div class="card-body p-3">'
-        + '<label class="text-secondary mb-0">Grade: &nbsp; </label>'
-        + '<label>' + offer.grade + '</label><br>'
-        + '<label class="text-secondary mb-0">Type of drug: &nbsp; </label>'
-        + '<label>' + offer.typeOfDrug + '</label><br>'
-        + '</div>' + button + '</div></div></div>'
+        + '<label class="text-secondary mb-0">Total price: &nbsp; </label>'
+        + '<label>' + offer.drugOfferDTO.totalPrice + '</label><br>'
+        + '<label class="text-secondary mb-0">Status: &nbsp; </label>'
+        + '<label>' + offer.drugOfferDTO.status + '</label><br>'
+        + '<label class="text-secondary mb-0">Delivery time: &nbsp; </label>'
+        + '<label>' + offer.drugOfferDTO.limitDate + '</label><br><br>'
+
+		+ ' <blockquote class="blockquote mb-0"><p><b>Order</b><br> Limit date: &nbsp; '
+	    + offer.pharmacyOrderDTO.limitDate + '</p>' 
+
+        + '</div> </div> </div> </div>'
+
     );
-    $('div#div_drugs').append(divElement);
+    $('div#div_offers').append(divElement);
 }
-
-
-function showSubstituteDrugs(offerId) {
-	substituteList = '';				
-    $.ajax({
-        url: '/api/drug/' + offerId + '/substitute',
-        type: 'GET',
-        dataType: 'json',
-        processData: false,
-        contentType: false,
-        success: function (substituteDrugs) {	
-            if (substituteDrugs.length == 0) {	
-				substituteList = 'No substitute drugs found.';
-				let table = '<table style="margin-left:20px; margin-right:20px; margin-top:25px; margin-bottom:25px; width:450px;">'
-					+ '<tr><td><b><strong> ' + substituteList + '</strong></b> </td></tr>'
-					+ '</table>';		
-				$('#substitute_content').empty();
-				$('#substitute_content').append(table);
-				$('#centerModalSuccess').modal('show');
-            }
-            else {
-				substituteList = substituteDrugs[0].name;
-                for (let i = 1; i < substituteDrugs.length; i++) {
-					substituteList = substituteList + ', ' + substituteDrugs[i].name;
-                }
-				let table = '<table style="margin-left:20px; margin-right:20px; margin-top:25px; margin-bottom:25px; width:450px;">'
-					+ '<tr><td><b><strong>Substitute drugs: </strong></b>' + substituteList + ' </td></tr>'
-					+ '</table>';		
-				$('#substitute_content').empty();
-				$('#substitute_content').append(table);
-				$('#centerModalSuccess').modal('show');	
-            }
-        },
-        error: function (jqXHR) {
-			let table = '<table style="margin-left:20px; margin-right:20px; margin-top:25px; margin-bottom:25px; width:300px;">'
-					+ '<tr><td> Error! ' + jqXHR.responseJSON + ' </td></tr>'
-					+ '</table>';		
-				$('#substitute_content').empty();
-				$('#substitute_content').append(table);
-				$('#centerModalSuccess').modal('show');
-        }
-    });			
-}
-
-
-function showSpecification(drugId) {	
-	ingredientsList = offerMap[drugId].ingredients[0].name;
-	for (let i = 1; i < offerMap[drugId].ingredients.length; i++) {		
-		ingredientsList = ingredientsList + ', ' + offerMap[drugId].ingredients[i].name;
-	}
-		
-	let table1 = '<table style="margin-left:20px; margin-right:20px; margin-top:25px; margin-bottom:25px; width:450px;">'
-		+ '<tr><td><b><strong>Contraindication: </strong></b>' + offerMap[drugId].contraindication + ' </td></tr>'
-		+ '<tr><td><b><strong>Daily dose: </strong></b>' + offerMap[drugId].dailyDose + '</td></tr>'
-		+ '<tr><td><b><strong>Ingredients: </strong></b>' + ingredientsList + ' </td></tr>'
-		+ '</table>';
-
-	$('#report_content').empty();
-	$('#report_content').append(table1);
-	$('#bottomModalSuccess').modal('show');
-};
-
 
 
