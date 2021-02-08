6759d5a40b5158bc068b31bbcaad2818fe44514a
Jelena Zeljko <jelena_zeljko@uns.ac.rs>
2021-02-08 20:20:43 +0100

    gui: created a form for adding the drug  (html and js)

--- isa-project/src/main/resources/static/js/supplier/drug_storage.js
+++ isa-project/src/main/resources/static/js/supplier/drug_storage.js
@@ -2,7 +2,45 @@ checkUserRole("ROLE_SUPPLIER");
 var supplierId = getUserIdFromToken();
 
 $(document).ready(function () {
+		
+	getDrugQuantityBySupplier();
 
+	getAllDrugs();	
+	
+	/*Update drug quantity on submit*/
+	$('form#updateDrugQuantity').submit(function (event) {	
+	
+		event.preventDefault();		
+		let drugId = $("#drug option:selected").attr("id");
+		let quantity = $('#quantity').val();
+		
+		var newDrugQuantity = {
+			"drugId": drugId,
+			"supplierId": supplierId,
+			"quantity": quantity 
+		}
+		/*
+		$.ajax({
+			url: "/api/patient",
+			type: 'POST',
+			contentType: 'application/json',
+			data: JSON.stringify(newDrugQuantity),
+			success: function () {
+				location.reload();
+				return;
+			},
+			error: function (jqXHR) {
+				console.log(jqXHR);
+				alert('Error update drug quantity!');
+				return;
+			}
+		});			*/		
+	});			
+});
+
+
+
+function getDrugQuantityBySupplier() {
 	$.ajax({
 		type:"GET", 
 		url: "/api/drug-quantity-supplier/" + supplierId + "/supplier",
@@ -15,22 +53,36 @@ $(document).ready(function () {
 				addDrugQuantityRow(quantities[i]);
 			}								
 		},
-		error:function(){
-			console.log('error getting drug quantity');
-			alert('error getting drug quantity');
+		error:function(jqXHR){
+			console.log(jqXHR);
+			alert('Error getting drug quantity');
 		}
-	});				
-});
+	});		
+}
 
 
-
-function addDrugQuantityRow(drug) {
-	
+function addDrugQuantityRow(drug) {	
 	let row = $('<tr><td>'+ drug.drugName +'</td><td>' + drug.quantity + '</td></tr>');	
 	$('#body_table').append(row);	
 }
 
 
-
-
-
+function getAllDrugs() {		
+    $.ajax({
+        url: '/api/drug',
+        type: 'GET',
+        dataType: 'json',
+        processData: false,
+        contentType: false,
+        success: function (drugs) {
+            for (let i = 0; i < drugs.length; i++) {
+            	$("select#drug").append('<option id="' + drug[i].id + '">' + drugs[i].name + '</option>');            
+            }
+        },
+        error: function (jqXHR) {
+			console.log(jqXHR);
+			alert("Error getting drugs!");
+        }
+    });	
+}
+	
