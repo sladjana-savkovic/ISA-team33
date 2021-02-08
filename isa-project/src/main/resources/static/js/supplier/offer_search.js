aa805771e65dc163ce3928f2128068a1acbc559c
Jelena Zeljko <jelena_zeljko@uns.ac.rs>
2021-02-08 02:02:25 +0100

    refactor: getting offers and orders by supplier id (frontend)

--- isa-project/src/main/resources/static/js/supplier/offer_search.js
+++ isa-project/src/main/resources/static/js/supplier/offer_search.js
@@ -1,7 +1,6 @@
 //ova stranica je dostupna dobavljacu
 
 supplierID = 8;
-offerMap = {};
 offersList = null;
 
 $(document).ready(function () {
@@ -9,6 +8,76 @@ $(document).ready(function () {
 	getAllOffersBySupplier();
 	
 });
+
+
+function getAllOffersBySupplier() {		
+    $.ajax({
+        url: '/api/drug-offer/all/' + supplierID + '/supplier',
+        type: 'GET',
+        dataType: 'json',
+        processData: false,
+        contentType: false,
+        success: function (offers) {
+			offersList = offers;
+            if (offers.length == 0) {
+                let alert = '<div id="loading" class="alert alert-info" role="alert"> No offers found. </div>';
+                $("#loading").hide();
+                $("#div_offers").prepend(alert);
+            }
+            else {
+                for (let i = 0; i < offers.length; i++) {
+                    addOfferCard(offers[i]);
+                }
+                $("#loading").hide();
+            }
+        },
+        error: function (jqXHR) {
+            let alert = '<div id="loading" class="alert alert-danger" role="alert"> Error! ' + jqXHR.responseJSON + '</div>';
+            $("#loading").hide();
+            $("#div_offers").prepend(alert);
+        }
+    });	
+}
+
+
+function addOfferCard(offer) {
+	
+	let drugQuantityList = offer.orderAndQuantityDTO.drugQuantityDTOs;
+	let drugQuantityListText = '';
+		
+	for (let i = 0; i < drugQuantityList.length; i++) {
+		let element = drugQuantityList[i].drugName + ': ' + drugQuantityList[i].quantity + '<br>';
+		drugQuantityListText = drugQuantityListText +  element;
+	}
+							
+    let divElement = $(
+        '<div class="row">'
+        + '<div class="col mb-4">'
+        + '<div class="card">'
+        + '<div class="card-header bg-info text-white">'
+        + '<h5 class="card-title mb-0"> Offer (ID = ' + offer.drugOfferDTO.id + ')</h5></div>'
+        + '<div class="card-body p-3">'
+        + '<label class="text-secondary mb-0">Total price: &nbsp; </label>'
+        + '<label>' + offer.drugOfferDTO.totalPrice + '</label><br>'
+        + '<label class="text-secondary mb-0">Status: &nbsp; </label>'
+        + '<label>' + offer.drugOfferDTO.status + '</label><br>'
+        + '<label class="text-secondary mb-0">Delivery time: &nbsp; </label>'
+        + '<label>' + offer.drugOfferDTO.limitDate + '</label><br><br><br>'
+
+		+ ' <blockquote class="blockquote mb-0 text-muted"><b>Order:</b><br><p> Limit date: &nbsp; '
+	    + offer.orderAndQuantityDTO.limitDate + '</p>' 
+		+ '<p>  Quantity: <br>' + drugQuantityListText + '</p>'		
+		+ '<footer class="blockquote-footer text-muted"><cite> Pharmacy: &nbsp;'
+		+ offer.orderAndQuantityDTO.pharmacyName + '</cite></footer>'		
+        + '</div> </div> </div> </div>'
+    );
+    $('div#div_offers').append(divElement);
+}
+
+
+
+//************************************************** 
+
 
 
 function searchOffer() {	
@@ -32,7 +101,7 @@ function searchOffer() {
             }
             else {
                 for (let i = 0; i < offers.length; i++) {
-                    addOfferRow(offers[i]);
+                    addOfferCard(offers[i]);
                 }
                 $("#loading").hide();
             }
@@ -46,61 +115,8 @@ function searchOffer() {
 }
 
 
-function getAllOffersBySupplier() {		
-    $.ajax({
-        url: '/api/drug-offer/all/' + supplierID + '/supplier',
-        type: 'GET',
-        dataType: 'json',
-        processData: false,
-        contentType: false,
-        success: function (offers) {
-			offersList = offers;
-            if (offers.length == 0) {
-                let alert = '<div id="loading" class="alert alert-info" role="alert"> No offers found. </div>';
-                $("#loading").hide();
-                $("#div_offers").prepend(alert);
-            }
-            else {
-                for (let i = 0; i < offers.length; i++) {
-                    addOfferRow(offers[i], i);
-                }
-                $("#loading").hide();
-            }
-        },
-        error: function (jqXHR) {
-            let alert = '<div id="loading" class="alert alert-danger" role="alert"> Error! ' + jqXHR.responseJSON + '</div>';
-            $("#loading").hide();
-            $("#div_offers").prepend(alert);
-        }
-    });	
-}
 
 
-function addOfferRow(offer, i) {
-		
-	offerMap[offer.id] = offer;
-				
-    let divElement = $(
-        '<div class="row">'
-        + '<div class="col mb-4">'
-        + '<div class="card">'
-        + '<div class="card-header bg-info text-white">'
-        + '<h5 class="card-title mb-0"> Offer (ID = ' + offer.drugOfferDTO.id + ')</h5></div>'
-        + '<div class="card-body p-3">'
-        + '<label class="text-secondary mb-0">Total price: &nbsp; </label>'
-        + '<label>' + offer.drugOfferDTO.totalPrice + '</label><br>'
-        + '<label class="text-secondary mb-0">Status: &nbsp; </label>'
-        + '<label>' + offer.drugOfferDTO.status + '</label><br>'
-        + '<label class="text-secondary mb-0">Delivery time: &nbsp; </label>'
-        + '<label>' + offer.drugOfferDTO.limitDate + '</label><br><br>'
 
-		+ ' <blockquote class="blockquote mb-0"><p><b>Order</b><br> Limit date: &nbsp; '
-	    + offer.pharmacyOrderDTO.limitDate + '</p>' 
-
-        + '</div> </div> </div> </div>'
-
-    );
-    $('div#div_offers').append(divElement);
-}
 
 
