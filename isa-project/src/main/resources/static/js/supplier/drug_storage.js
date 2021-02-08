b771d7bb3239eb97eafe992baf58098bab99a0c1
Jelena Zeljko <jelena_zeljko@uns.ac.rs>
2021-02-08 18:30:08 +0100

    gui: the supplier sees drug quantity

--- isa-project/src/main/resources/static/js/supplier/drug_storage.js
+++ isa-project/src/main/resources/static/js/supplier/drug_storage.js
@@ -3,7 +3,34 @@ var supplierId = getUserIdFromToken();
 
 $(document).ready(function () {
 
-	alert("drug storage, supplier = " + supplierId);
-
-	
+	$.ajax({
+		type:"GET", 
+		url: "/api/drug-quantity-supplier/" + supplierId + "/supplier",
+		headers: {
+            'Authorization': 'Bearer ' + window.localStorage.getItem('token')
+        },
+		contentType: "application/json",
+		success:function(quantities){					
+			for(i = 0; i < quantities.length; i++) {
+				addDrugQuantityRow(quantities[i]);
+			}								
+		},
+		error:function(){
+			console.log('error getting drug quantity');
+			alert('error getting drug quantity');
+		}
+	});				
 });
+
+
+
+function addDrugQuantityRow(drug) {
+	
+	let row = $('<tr><td>'+ drug.drugName +'</td><td>' + drug.quantity + '</td></tr>');	
+	$('#body_table').append(row);	
+}
+
+
+
+
+
