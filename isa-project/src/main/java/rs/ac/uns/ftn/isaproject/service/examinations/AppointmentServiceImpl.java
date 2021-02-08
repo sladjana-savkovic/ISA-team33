a0446bdbc56e24fe492f64fd19a3755b57ae65af
jelenabudisa <jelena.budisa@uns.ac.rs>
2021-02-08 22:17:32 +0100

    fix: small fix

--- isa-project/src/main/java/rs/ac/uns/ftn/isaproject/service/examinations/AppointmentServiceImpl.java
+++ isa-project/src/main/java/rs/ac/uns/ftn/isaproject/service/examinations/AppointmentServiceImpl.java
@@ -238,6 +238,7 @@ public class AppointmentServiceImpl implements AppointmentService {
 		return false;
 	}
 	@Override
+	@Transactional(readOnly = false)
     public boolean isDoctorAvailableForChosenTime(int doctorId, LocalDate date, LocalTime startTime, LocalTime endTime) {
         Collection<Appointment> doctorAppointments = getCreatedAndScheduledDoctorAppointments(doctorId);
         return !checkIfAppointmentMathces(doctorAppointments, date, startTime, endTime);
